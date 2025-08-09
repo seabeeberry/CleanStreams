ad6183789f422dfab757d119fc493af664fd1f16
seabeeberry <seri@Mac.fritz.box>
2025-08-09 15:42:22 +0200

    Implemented popup.js to set storage variables dependent on user's choices

--- popup.js
+++ popup.js
@@ -1,3 +1,20 @@
-// TODO: Collect current settings in chrome.storage.sync
+chrome.storage.sync.get(
+  ['hideCategoryHotTub', 'hideCategoryASMR', 'hideHypeTrain'],
+  (data) => {
+    document.getElementById('hideCategoryHotTub').checked = !!data.hideCategoryHotTub;
+    document.getElementById('hideCategoryASMR').checked = !!data.hideCategoryASMR;
+    document.getElementById('hideHypeTrain').checked = !!data.hideHypeTrain;
+  }
+);
 
-// TODO: When a checkbox changes, save that setting to chrome.storage.sync
+document.getElementById("hideCategoryHotTub").addEventListener("change", function() {
+    chrome.storage.sync.set({ hideCategoryHotTub: this.checked });
+})
+
+document.getElementById("hideCategoryASMR").addEventListener("change", function() {
+    chrome.storage.sync.set({ hideCategoryASMR: this.checked });
+})
+
+document.getElementById("hideHypeTrain").addEventListener("change", function() {
+    chrome.storage.sync.set({ hideHypeTrain: this.checked });
+})
